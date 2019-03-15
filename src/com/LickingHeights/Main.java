7f99427d546cd6950242f342d5751b6aec53b3c2
Alex Dome <alex.dome.2022@student.lhschools.org>
2019-03-15 14:09:43 -0400

    Finished final method and added dialogs

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,66 +1,55 @@
 package com.LickingHeights;
-
 import java.util.Scanner;
-
 public class Main {
-
     public static void main(String[] args) {
         //declare
         Scanner keyboard;
-        keyboard = new Scanner(System.in);
-
+            keyboard = new Scanner(System.in);
         //initialize
         String noun2;
-        noun2 = ("World");
-        String userName;
-        int firstNumber;
-        int secondNumber;
-        int thirdNumber;
-        int fourthNumber;
-
+            noun2 = ("World");
+                String userName;
+                    int firstNumber, secondNumber,
+                        thirdNumber, fourthNumber;
         //code
         System.out.println("What is your name?");
-        userName = keyboard.nextLine();
-        System.out.println("Give me a number please");
-        firstNumber = keyboard.nextInt();
-        System.out.println("Another please");
-        secondNumber = keyboard.nextInt();
-        System.out.println("Can I have another number please");
-        thirdNumber = keyboard.nextInt();
-        System.out.println("One last number please");
-        fourthNumber = keyboard.nextInt();
-
+            userName = keyboard.nextLine();
+                System.out.println("Give me a number please");
+                    firstNumber = keyboard.nextInt();
+                        System.out.println("Another please");
+                            secondNumber = keyboard.nextInt();
+                        System.out.println("Can I have another number please");
+                    thirdNumber = keyboard.nextInt();
+                System.out.println("One last number please");
+            fourthNumber = keyboard.nextInt();
+        System.out.println("Thank you very much");
+        //Methods
         //Method 1
         printGreetings("World");
-
         //Method 2
         helloName("" + userName);
-
         //Method 3
-        System.out.println(firstNumber * secondNumber);
-
+        System.out.println("Im going to multiply your First and Second Numbers");
+                System.out.println(firstNumber * secondNumber);
         //Method 4
-        System.out.println(thirdNumber / fourthNumber);
-
+        System.out.println("Now im going to divide your Third and Fourth Numbers");
+            System.out.println(thirdNumber / fourthNumber);
         //Method 5
-        System.out.println(firstNumber ^ ((firstNumber ^ thirdNumber)& -(firstNumber << thirdNumber)));
+        System.out.println("Finally im going to find the maximum of your First and Third Numbers");
+            System.out.println(firstNumber ^ ((firstNumber ^ thirdNumber)& -(firstNumber << thirdNumber)));
         //Method 6
         change("" + noun2);
     }
-
     private static void printGreetings(String noun) {
         System.out.println("Hello " + noun);
     }
-
     private static void helloName(String userName) {
         System.out.println("Hello " + userName);
     }
-
     public static int multiply(int firstNumber, int secondNumber) {
         int answer = firstNumber * secondNumber;
         return answer;
     }
-
     public static int divide(int thirdNumber, int fourthNumber) {
         int answer2 = thirdNumber / fourthNumber;
         return answer2 * 2;
